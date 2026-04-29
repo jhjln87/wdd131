4b76172e93ec1372c41431acd5d093659c9f5e4d
John <60108868+jhjln87@users.noreply.github.com>
2026-04-29 14:30:33 -0600

    finish dom

--- ponder/week2/dom.js
+++ ponder/week2/dom.js
@@ -1,9 +1,15 @@
-/* proviced practice code */
-// let selectElem = document.getElementById('webdevlist');
-// selectElem.addEventListener('change', function(){
-//     let codeValue = selectElem.value;
-//     console.log(codeValue);
-// })
+/* proviced homework code */
+let selectElem = document.getElementById('webdevlist');
+let html = document.getElementById("html");
+let css = document.getElementById("css");
+let js = document.getElementById("js");
+selectElem.addEventListener('change', function(){
+    html.style.fontSize = "1.5em";
+    css.style.fontSize = "1.5em";
+    js.style.fontSize = "1.5em";
+    let codeValue = selectElem.value;
+    document.getElementById(codeValue).style.fontSize = "1.8em";
+})
 
 /* play time */
 // let title = document.querySelector("h1");
@@ -14,16 +20,16 @@
 // });
 
 /* intro */
-let title = document.querySelector('h1');
-console.log (title);
-title.textContent = 'Web Page Components';
-const topics = document.querySelector('#topics');
-topics.style.color = "white";
-const wrapper = document.getElementById('content');
-wrapper.style.backgroundColor = "black";
-let list = document.querySelector(".list");
-list.style.border = "3px solid black";
-let para = document.querySelector('p');
-para.style.fontSize = '2em';
-const image = document.querySelector('img');
-image.setAttribute('src', 'darkmode.jpg');
+// let title = document.querySelector('h1');
+// console.log (title);
+// title.textContent = 'Web Page Components';
+// const topics = document.querySelector('#topics');
+// topics.style.color = "white";
+// const wrapper = document.getElementById('content');
+// wrapper.style.backgroundColor = "black";
+// let list = document.querySelector(".list");
+// list.style.border = "3px solid black";
+// let para = document.querySelector('p');
+// para.style.fontSize = '2em';
+// const image = document.querySelector('img');
+// image.setAttribute('src', 'darkmode.jpg');
