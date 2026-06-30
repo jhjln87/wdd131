59dccabd340f01f9d8a97740b02acc230a3f7488
John <60108868+jhjln87@users.noreply.github.com>
2026-06-29 20:10:06 -0600

    finish form1

--- final/listSorter/js.js
+++ final/listSorter/js.js
@@ -3,41 +3,49 @@ let people = [
         name: 'John',
         age: 22,
         height_in: 67,
+        gender: 'M',
     },
     {
         name: 'Kaity',
         age: 27,
         height_in: 62,
+        gender: 'F',
     },
     {
         name: 'Wemby',
         age: 22,
         height_in: 88,
+        gender: 'M',
     },
     {
         name: 'Johnathan',
         age: 23,
         height_in: 72,
+        gender: 'M',
     },
     {
         name: 'Yedam',
         age: 27,
         height_in: 70,
+        gender: 'M',
     },
     {
         name: 'Alex',
         age: 21,
         height_in: 69,
+        gender: 'M',
     },
     {
         name: 'Jacob', 
         age: 18,
         height_in: 66,
+        gender: 'M',
     },
     {
         name: 'Tyler',
         age: 32,
         height_in: 76,
+        gender: 'M',
     },
 ]
 
@@ -66,13 +74,19 @@ function findMax(method) {
 
 function template(person, method, min, max) {
     let retrieve = person[method];
+    let color;
+    if (person.gender == "M") {
+        color = "#C6E2FF";
+    } else {
+        color = "#FFB6C1";
+    }
     switch (method) {
         case 'name':
-            return `<span style="height:100%"><p>${retrieve}</p></span>`;
+            return `<span style="height:100%; background-color:${color}"><p>${retrieve}</p></span>`;
             break;
         case 'age': case 'height_in':
             let percentage = ((retrieve - min) / (max - min)) * 75 + 25;
-            return `<span style="height:${percentage}%"><p>${person.name}</p></span>`;
+            return `<span style="height:${percentage}%; background-color:${color}"><p>${person.name}</p></span>`;
             break;
     }
 }
