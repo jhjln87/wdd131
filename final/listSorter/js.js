721ba017815b5376b99fbb27e2ace8a00d3401fe
John <60108868+jhjln87@users.noreply.github.com>
2026-06-30 11:08:25 -0600

    fewmorethangs

--- final/listSorter/js.js
+++ final/listSorter/js.js
@@ -50,7 +50,12 @@ function template(person, method, min, max) {
             return `<span style="height:100%; background-color:${color}"><p>${retrieve}</p></span>`;
             break;
         case 'age': case 'height_in':
-            let percentage = ((retrieve - min) / (max - min)) * 75 + 25;
+            let percentage;
+            if (min == max) {
+                percentage = 100;
+            } else {
+                percentage = ((retrieve - min) / (max - min)) * 75 + 25;
+            }
             return `<span style="height:${percentage}%; background-color:${color}"><p>${person.name}</p></span>`;
             break;
     }
@@ -64,6 +69,28 @@ function render(method) {
 }
 
 
+function compareObj(method) {
+    return function(a, b) {
+        if (a[method] < b[method]) {
+            return -1;
+        } else if (a[method] > b[method]) {
+            return 1;
+        }
+        return 0;
+    };
+}
+
+function sortList(method) {
+    switch (method) {
+        case 'name':
+            return people.sort(compareObj('name'));
+            break;
+        case 'age': case 'height_in':
+            return people.sort(compareObj(method));
+            break;
+    }
+}
+
 select.addEventListener('change', (item) => {
     sortBy = item.target.value
     render(sortBy); 
@@ -74,34 +101,52 @@ sortForm.addEventListener('submit', (event) => {
     let selected = sortForm.querySelector('input[name="sortBy"]:checked');
     if (selected) {
         sortBy = selected.value;
+        sortList(sortBy)
         render(sortBy);
     }
 });
 
 addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
     const nameInput = document.getElementById('name');
     const ageInput = document.getElementById('age');
     const heightInput = document.getElementById('height');
     const genderInput = document.querySelector('input[name="gender"]:checked');
-    
+    if (nameInput.value === "test" && parseInt(ageInput.value) === 0) {
+        admin();
+        render(sortBy);
+        addForm.reset();
+        return;
+    }
     let newPerson = {
         name: nameInput.value,
         age: parseInt(ageInput.value),
         height_in: parseInt(heightInput.value),
         gender: genderInput.value
     };
-    
     people.push(newPerson);
     render(sortBy);
     addForm.reset();
 });
 
 
+function admin() {
+    people.push({name:"John", age:22, height_in:68, gender:"M"});
+    people.push({name:"Wemby", age:22, height_in:88, gender:"M"});
+    people.push({name:"Kaitlyn", age:27, height_in:62, gender:"F"});
+    people.push({name:"Yedam", age:27, height_in:70, gender:"M"});
+    people.push({name:"Johnathan", age:23, height_in:72, gender:"M"});
+    people.push({name:"Alex", age:20, height_in:67, gender:"M"});
+    people.push({name:"Jacob", age:18, height_in:66, gender:"M"});
+    people.push({name:"Woman", age:25, height_in:64, gender:"F"});
+}
+
+
 function init() {
     addForm.classList.add('hide');
     sortForm.classList.add('hide');
     sortBy = "height_in";
-    render(sortBy);
+    listContainer.innerHTML = `<h3>Your list is empty!</h3>`
 }
 
 init();
